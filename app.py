1640ccf0616fb7d40cb5d69598b2d05d19e90872
Alécio <alec1o@protonmail.com>
2022-05-29 20:18:04 +0300

    create sample 'Hello World' project

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-ÿþ
+from flask import Flask
+from decouple import config
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Index"
+
+
+if __name__ == "__main__":
+    app.run(port=config("PORT", 80), debug=True)
