a3b68f564ec51ab7b06176bf619c4f5b6b690bed
Alécio <alec1o@protonmail.com>
2022-06-02 18:12:02 +0300

    add button for horizontal scroll bar

--- src/www/js/inc/image.js
+++ src/www/js/inc/image.js
@@ -1,16 +1,34 @@
+let background_size = 0;
+let poster_size = 0;
+
 window.onresize = () => {
 	OnResizePoster();
 };
 
 OnResizePoster();
 
+function PosterButton(type, dir, self) {
+	let s = document.querySelector("aa");
+	if (self) s = self;
+	const poster = s.parentElement.parentElement.querySelector(".poster");
+
+	if (type && dir && self) {
+		if (type === "background") {
+			poster.scrollLeft += background_size * dir;
+		} else if (type == "poster") {
+			poster.scrollLeft += poster_size * dir;
+		}
+	}
+}
+
 function OnResizePoster() {
 	try {
-		const all = document.querySelectorAll(".image--poster");
+		const all = document.querySelectorAll(".image_poster");
 
 		all.forEach((image) => {
 			const oldHeight = image.clientHeight;
 			const newHeight = image.clientWidth * 1.5;
+			poster_size = image.clientWidth;
 
 			const target = image.querySelector(".poster_1");
 
@@ -27,6 +45,7 @@ function OnResizePoster() {
 		all.forEach((image) => {
 			const oldHeight = image.clientHeight;
 			const newHeight = image.clientWidth * 0.57;
+			background_size = image.clientWidth;
 
 			const target = image.querySelector(".poster_1");
 
@@ -37,5 +56,27 @@ function OnResizePoster() {
 		});
 	} catch {}
 
-	console.log("OnResize Poster");
+	try {
+		const all = document.querySelectorAll(".poster_container");
+
+		all.forEach((poster_container) => {
+			const buttons = poster_container.querySelector(".poster_buttons");
+			const poster = poster_container.querySelector(".poster");
+			let removeHeight = 0;
+
+			if (poster) {
+				const poster_2 = poster.querySelector(".poster_2");
+				removeHeight = poster_2.clientHeight;
+			}
+
+			if (buttons) {
+				const calc = (poster.clientHeight - removeHeight) / 2;
+				buttons.style.marginTop = `${calc}px`;
+				buttons.style.height = `${0}px`;
+				buttons.style.width = `${poster_container.clientWidth}px`;
+			}
+		});
+	} catch {}
+
+	console.log("OnResizePoster...");
 }
