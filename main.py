6a06aae02e51d8e5e3a4deae814c7662fcaf6a36
Alecio <alec1o@protonmail.com>
2022-07-22 15:08:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from Global import *
 
 app = Flask(__name__, static_folder='www/', template_folder='views/')
 api = None  # Api(app)
+api_url = config("API_URL", "http://localhost:5206")
 
-Global.init(app, api)
+Global.init(app, api, api_url)
 Global.init_controllers()
 
 if __name__ == "__main__":
