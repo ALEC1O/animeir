9d3611e302a2339f48ebfef9cec05902666a5ea5
Alecio <alec1o@protonmail.com>
2022-07-22 15:08:20 +0200

    add api url in global

--- Global.py
+++ Global.py
@@ -1,14 +1,15 @@
 from decouple import config
-from flask import Flask, render_template as render
+from flask import Flask, render_template as render, request, redirect, url_for
 from flask_restx import Api, Resource
 
 
 class Global:
 
     @staticmethod
-    def init(app: Flask, api: Api):
+    def init(app: Flask, api: Api, api_url: str):
         Global.App = app
         Global.Api = api
+        Global.ApiUrl = api_url
 
         Global.AppKey = config("APP_kEY", "")
         Global.ApiKey = config("API_KEY", "")
